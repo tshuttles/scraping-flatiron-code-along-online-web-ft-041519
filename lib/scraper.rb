adc3c87e7de8f4c8ac6a05f8a269fdab5006281e
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-10 16:59:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,7 @@ class Scraper
     end
   
   def get_page 
-    Nokogiri::HTML(open(http://learn-co-curriculum.github.io/site-for-scraping/courses))
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end 
 end
 
