592f55d1d63d07953892c4fd136c4360923929af
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-10 16:47:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,9 +12,9 @@ class Scraper
         puts "Title: #{course.title}"
         puts "  Schedule: #{course.schedule}"
         puts "  Description: #{course.description}"
+        end
       end
     end
-  end
   
 end
 
