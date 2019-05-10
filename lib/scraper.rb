21d6ddda77dabb2ce39d4226b2ac089543c0ec30
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-10 18:32:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,12 +33,7 @@ class Scraper
   end 
   
   def make_courses
-    doc.css(".post").each do |post|
-      course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
-    end
+    
   end 
 end
 
